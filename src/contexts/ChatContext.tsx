9cc8bd32ab66cf2b38a477d647eae12d4d018ff9
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-08 19:32:20 -0300

    feat: create chat context

---
+++ src/contexts/ChatContext.tsx
@@ -0,0 +1,26 @@
+import { chatReducer } from "@/reducers/chatReducer";
+import { createContext, ReactNode, useContext, useReducer } from "react";
+import { MessageProps } from "../types/message";
+
+type ChatContextProps = {
+  chat: MessageProps[];
+  addMessage: (user: string, text: string) => void;
+};
+
+const ChatContext = createContext<ChatContextProps | null>(null);
+
+export const ChatProvider = ({ children }: { children: ReactNode }) => {
+  const [chat, dispatch] = useReducer(chatReducer, []);
+
+  const addMessage = (user: string, text: string) => {
+    dispatch({ type: "add", payload: { user, text } });
+  };
+
+  return (
+    <ChatContext.Provider value={{ chat, addMessage }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => useContext(ChatContext);
---
+++ src/types/message.ts
@@ -0,0 +1,5 @@
+export type MessageProps = {
+  id: number;
+  user: string;
+  text: string;
+};
