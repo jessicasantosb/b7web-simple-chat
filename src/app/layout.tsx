ef725695b36b2c3a25b7b2df0ab1508164e230ab
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-08 15:39:12 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chat simple",
+  description:
+    "Este é um chat simples desenvolvido no curso de ReactJS da B7Web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
