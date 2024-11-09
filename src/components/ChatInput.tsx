32b905750e7339c07f1b8c586689328eaf67f561
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-09 09:54:29 -0300

    feat: handle sending message

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useChat } from "@/contexts/ChatContext";
+import { KeyboardEvent, useState } from "react";
 
 export function ChatInput({ name }: { name: string }) {
   const [message, setMessage] = useState("");
+  const chatCtx = useChat();
+
+  const handleKeyUpAction = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code.toLowerCase() !== "enter" || message.trim() === "") return;
+    chatCtx?.addMessage(name, message.trim());
+    setMessage("");
+  };
 
   return (
     <input
@@ -11,6 +19,7 @@ export function ChatInput({ name }: { name: string }) {
       value={message}
       onChange={(e) => setMessage(e.target.value)}
       placeholder={`${name}, digite sua mensagem`}
+      onKeyUp={handleKeyUpAction}
     />
   );
 }
